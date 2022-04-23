8ed0456967e17dd9c6dec0b018a2e4e08743e06c
Bas-sheikh <basilelshakhe@gmail.com>
2022-04-23 05:29:14 +0300

    add setInterval funtion that inlcudes 12 variable for call all elmenets that i will use it for clock and make some of logic for conect between js and css such as transform rotate and stroke dash offset relates #4

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // clock script start
+
+setInterval(() => {
+  let hourH = document.getElementById(`hour-h1`);
+  let mintH = document.getElementById(`mint-h1`);
+  let secnH = document.getElementById(`secn-h1`);
+  
+  let hourCir = document.getElementById(`hour-cir`);
+  let mintCir = document.getElementById(`mint-cir`);
+  let secnCir = document.getElementById(`secn-cir`);
+  
+  let hourBall = document.getElementById(`hour`);
+  let mintBall = document.getElementById(`mint`);
+  let secnBall = document.getElementById(`secn`);
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  if (h > 12) {
+    h = h - 12;
+  }
+
+  h = (h < 10) ? `0` + h : h;
+  m = (m < 10) ? `0` + m : m;
+  s = (s < 10) ? `0` + s : s;
+
+  hourH.textContent = h;
+  mintH.textContent = m;
+  secnH.textContent = s;
+
+  hourCir.style.strokeDashoffset = 440 - (440 * h) / 12;
+  mintCir.style.strokeDashoffset = 440 - (440 * m) / 60;
+  secnCir.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+  hourBall.style.transform = `rotate(${h * 30}deg)`;
+  // 360 / 12 = 30
+  mintBall.style.transform = `rotate(${m * 6}deg)`;
+  // 360 / 60 = 6
+  secnBall.style.transform = `rotate(${s * 6}deg)`;
+  // 360 / 60 = 6
+})
